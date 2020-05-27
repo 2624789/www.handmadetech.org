c9628e4a98716cdc138c085ee96494ded46e3dd6
2624789 <2624789@users.noreply.github.com>
2020-05-27 06:19:54 -0500

    website init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,50 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <h1>HANDMADETECH</h1>
+
+      <p>Software bien diseñado, confiable y de calidad.</p>
+
+      <p>Well-crafted, trustworthy and quality software.</p>
+
+      <h2>¿Qué hacemos?</h2>
+
+      <p>
+        Definición, desarrollo, operación y mantenimiento de sistemas
+        software con un enfoque sistemático, disciplinado y cuantificable.
+      </p>
+
+      <p>
+        Principios y metodologías que se han demostrado adecuadas sobre
+        la base de la experiencia previa.
+      </p>
+
+      <h3>1. Definición</h3>
+
+      <p>PRD - Product Requirements Document</p>
+
+      <ul>
+        <li>¿Qué hace el producto?</li>
+        <li>Descripción</li>
+        <li>Métricas</li>
+        <li>Planeación trimestral OKRs</li>
+      </ul>
+
+      <h3>2. Desarrollo</h3>
+
+      <p>UI/X + Code</p>
+
+      <h3>3. Operación y mantenimiento</h3>
+
+      <p>SLAs - Service Level Agreements</p>
+
+      <h2>Más información</h2>
+
+      <p>info@handmadetech.org</p>
+
+      <p><strong>*BTC-friendly</strong></p>
+    </div>
+  );
 }
